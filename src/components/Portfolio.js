3afa6a3e8cae3b11d20708e25c1072dbdc2ae26a
taenam <tkim356@wisc.edu>
2024-09-04 16:17:29 -0500

    Portfolio project description updated

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,7 +11,7 @@ const Portfolio = () => {
             <img src="/images/work-1.png" alt="Social-To-Do" />
             <div className="layer">
               <h3>Social-To-Do</h3>
-              <p>A React Native application that uniquely merges task management with social media features</p>
+              <p>React Native app designed to seamlessly blend productivity with social engagement. It offers a unique platform where users can not only manage their tasks efficiently but also connect with friends, share progress, and interact through a social feed.</p>
               <a href="https://github.com/A9ine/Social-To-Do" target="_blank" rel="noopener noreferrer">
                 <img src="/images/github3.png" alt="GitHub link" className="github-icon" />
               </a>
@@ -21,7 +21,7 @@ const Portfolio = () => {
             <img src="/images/work-2.png" alt="Beyond the Book" />
             <div className="layer">
               <h3>Beyond the Book</h3>
-              <p>A project I led at HackIOWA, is a Python Gradio-based application to </p>
+              <p>A project I led at HackIOWA, Python-powered application built with Gradio that analyzes hidden university costs through data processing and visualization, helping students make informed financial decisions by revealing overlooked expenses like rent and groceries. </p>
               <a href="https://github.com/washedPat/beyond_the_book" target="_blank" rel="noopener noreferrer">
                 <img src="/images/github3.png" alt="GitHub link" className="github-icon" />
               </a>
@@ -31,7 +31,7 @@ const Portfolio = () => {
             <img src="/images/go.webp" alt="TaskMaster: Go" />
             <div className="layer">
               <h3>TaskMaster: Go</h3>
-              <p>Lightweight and secure task management API built with Go.</p>
+              <p>A secure RESTful API built with Go, offering JWT-based user authentication and PostgreSQL-powered task management with full CRUD functionality.</p>
               <a href="https://github.com/taenam1214/TaskMaster-Go" target="_blank" rel="noopener noreferrer">
                 <img src="/images/github3.png" alt="GitHub link" className="github-icon" />
               </a>
@@ -41,7 +41,7 @@ const Portfolio = () => {
             <img src="/images/work-3.jpeg" alt="Crypto Wallet Adoption" />
             <div className="layer">
               <h3>Crypto Wallet Adoption</h3>
-              <p>Utilized Python for data analysis to identify key factors driving adoption...</p>
+              <p>Used Python and CSV for data analysis to identify key factors driving crypto wallet adoption, revealing correlations with internet access, economic growth, education, and financial inclusion to inform targeted user engagement strategies.</p>
               <a href="https://github.com/taenam1214/CryptoWalletAdoption" target="_blank" rel="noopener noreferrer">
                 <img src="/images/github3.png" alt="GitHub link" className="github-icon" />
               </a>
